4848b44d32ae938498ae6c1fa997fd4f21e4fcd6
Alexander <respawn4@mail.ru>
2025-06-23 09:49:38 +0600

    added parse to protobuf methods

---
+++ pkg/grpcPb/parseToPb.go
@@ -0,0 +1,37 @@
+package grpcPb
+
+import "github.com/whoami00911/Audit-web-app-service/internal/domain"
+
+var (
+	actions = map[string]LogRequest_Actions{
+		"SignUp": LogRequest_SignUp,
+		"SignIn": LogRequest_SignIn,
+		"Logout": LogRequest_Logout,
+		"Upload": LogRequest_Upload,
+	}
+
+	methods = map[string]LogRequest_Methods{
+		"GET":    LogRequest_GET,
+		"PUT":    LogRequest_PUT,
+		"POST":   LogRequest_POST,
+		"DELETE": LogRequest_DELETE,
+	}
+)
+
+func ToPbAction(action string) (LogRequest_Actions, error) {
+	value, ok := actions[action]
+	if !ok {
+		return -1, domain.ErrNoAction
+	}
+
+	return value, nil
+}
+
+func ToPbMethod(method string) (LogRequest_Methods, error) {
+	value, ok := methods[method]
+	if !ok {
+		return -1, domain.ErrNoAction
+	}
+
+	return value, nil
+}
