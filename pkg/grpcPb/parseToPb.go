f52d3ce9e4453f91e32a66d70a487205cea86e97
Alexander <respawn4@mail.ru>
2025-06-24 14:51:43 +0600

    some fixes in parse methods

--- pkg/grpcPb/parseToPb.go
+++ pkg/grpcPb/parseToPb.go
@@ -4,10 +4,14 @@ import "github.com/whoami00911/Audit-web-app-service/internal/domain"
 
 var (
 	actions = map[string]LogRequest_Actions{
-		"SignUp": LogRequest_SignUp,
-		"SignIn": LogRequest_SignIn,
-		"Logout": LogRequest_Logout,
-		"Upload": LogRequest_Upload,
+		"SignUp":      LogRequest_SignUp,
+		"SignIn":      LogRequest_SignIn,
+		"Logout":      LogRequest_Logout,
+		"Upload":      LogRequest_Upload,
+		"GetFile":     LogRequest_Get_file,
+		"GetFiles":    LogRequest_Get_files,
+		"DeleteFile":  LogRequest_Delete_file,
+		"DeleteFiles": LogRequest_Delete_files,
 	}
 
 	methods = map[string]LogRequest_Methods{
