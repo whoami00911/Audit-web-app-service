d83ea5bdcce4dbd5f6c19f1ea0f67cb99f970c1e
Alexander <respawn4@mail.ru>
2025-06-27 17:48:34 +0600

    some fixes with time3

--- pkg/logEntities/logEntities.go
+++ pkg/logEntities/logEntities.go
@@ -7,22 +7,22 @@ type Status struct {
 }
 
 type Log struct {
-	Action          string   `bson:"action"`
-	Method          string   `bson:"method"`
-	UserId          int      `bson:"userId"`
-	ObjectId        []string `bson:"objectId,omitempty"`
-	Url             string   `bson:"url"`
-	Timestamp       time.Time
-	TimestampString string `bson:"timestamp"`
+	Action          string    `bson:"action"`
+	Method          string    `bson:"method"`
+	UserId          int       `bson:"userId"`
+	ObjectId        []string  `bson:"objectId,omitempty"`
+	Url             string    `bson:"url"`
+	Timestamp       time.Time `bson:"-"`
+	TimestampString string    `bson:"timestamp"`
 }
 
 type GinLog struct {
-	TimestampString string `bson:"timestamp"`
-	Timestamp       time.Time
-	StatusCode      int    `bson:"statusCode"`
-	Latency         string `bson:"latency"`
-	ClientIp        string `bson:"clientIp"`
-	Method          string `bson:"method"`
-	Path            string `bson:"path"`
-	UserAgent       string `bson:"userAgent"`
+	TimestampString string    `bson:"timestamp"`
+	Timestamp       time.Time `bson:"-"`
+	StatusCode      int       `bson:"statusCode"`
+	Latency         string    `bson:"latency"`
+	ClientIp        string    `bson:"clientIp"`
+	Method          string    `bson:"method"`
+	Path            string    `bson:"path"`
+	UserAgent       string    `bson:"userAgent"`
 }
